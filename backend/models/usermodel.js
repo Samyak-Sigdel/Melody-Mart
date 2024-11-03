ca922d2f4e3f66b9cf666aa0aa597944c84c6f98
SamyakSigdel <samyaksigdel77@gmail.com>
2024-11-03 14:36:03 +0545

    User Schema

--- backend/models/usermodel.js
+++ backend/models/usermodel.js
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 
 const userSchema = new  mongoose.Schema({
     name:{type:String,required:true},
-    
+    email:{type:String,required:true,unique:true},
+    password:{type:String,required:true},
+    date:{type:Date,default:Date.now},
     
 })
