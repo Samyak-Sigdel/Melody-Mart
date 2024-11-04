70b4230c79523c5bf9d1905578b05831fc8a0405
SamyakSigdel <samyaksigdel77@gmail.com>
2024-11-04 19:25:34 +0545

    product model

--- backend/models/productmodel.js
+++ backend/models/productmodel.js
@@ -2,5 +2,14 @@ import mongoose from "mongoose";
 
 const productSchema = new  mongoose.Schema({
     name:{type:String,required:true},
-    
+    id:{ type: Number,required:true,},
+    name:{type:String, required:true,},
+    image:{type:String,required:true,},
+    category:{type:String,required:true,},
+    priceOptions: [
+        { duration: {type: String,required: true },
+          price: {type: Number, required: true}
+        }
+      ],
+      description: { type: String, trim: true},
 })
